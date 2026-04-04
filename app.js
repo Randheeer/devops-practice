51d590c59eacfdd71bf8bd5faa0b7c2967b8bc92
root <root@LAPTOP-UU94EJDU.localdomain>
2026-04-04 20:57:47 +0000

    updates message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello DevOps World 🚀");
+  res.end("Hello DevOps Master World 🚀");
 });
 
 server.listen(3000, () => {
