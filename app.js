b12906e165248d381e2ef319cd25e7f0c1f0d027
root <root@LAPTOP-UU94EJDU.localdomain>
2026-04-05 13:19:16 +0000

    added github actions

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const { MongoClient } = require('mongodb');
 
-const server = http.createServer((req, res) => {
-  res.end("Hello DevOps Master World 🚀");
-});
+const url = process.env.MONGO_URL ||  "mongodb://mongo:27017";
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+async function connectWithRetry() {
+  const client = new MongoClient(url);
+
+  while (true) {
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB");
+      return client;
+    } catch (err) {
+      console.log("MongoDB not ready, retrying...");
+      await new Promise(res => setTimeout(res, 2000));
+    }
+  }
+}
+
+async function startServer() {
+  const client = await connectWithRetry();
+
+  const server = http.createServer((req, res) => {
+    res.end("App + MongoDB connected 🚀");
+  });
+
+  server.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
+
+startServer();
